d55c52ffa3c03210fe1b98260dd996bfd69f22bc
周麒麟 <mrkylin.zhou@gmail.com>
2017-09-08 22:53:14 +0800

    add session & redis

--- index.js
+++ index.js
@@ -3,14 +3,36 @@ const path = require('path');
 const router = require('koa-router')();
 const body = require('koa-body');
 const json = require('koa-json');
+const serve = require('koa-static');
+const redis = require('koa-redis');
+const session = require('koa-generic-session');
 const requireAll = require('require-all');
 const errorHandle = require('./lib/errorHandle');
 
-const server = new Koa();
+const app = new Koa();
 
 const isDev = process.env.NODE_ENV === 'development';
 
 const config = require('./config/config.json');
+
+app.keys = config.keys;
+
+const sessionConfig = {
+    key: `${config.appName}.sid`,
+    prefix: `${config.appName}:sess:`,
+    cookie: {
+        domain: '.zhouqilin.cn'
+    }
+};
+
+sessionConfig.store = redis({
+    host: config.redis.host,
+    port: config.redis.port
+});
+
+sessionConfig.store.on('error', error => {
+    console.error(error);
+});
 
 requireAll({
     dirname: path.join(__dirname, '/route'),
@@ -20,20 +42,20 @@ requireAll({
     }
 });
 
-server.use(errorHandle);
-server.use(body());
-server.use(json());
-
+app.use(errorHandle);
+app.use(session(sessionConfig));
+app.use(body());
+app.use(json());
 if (isDev) {
     const reqTimeLogger = require('./lib/reqTimeLogger');
     const logger = require('koa-logger');
-    server.use(logger());
-    server.use(reqTimeLogger);
+    app.use(logger());
+    app.use(reqTimeLogger);
 }
+app.use(serve(path.join(__dirname, '/static')));
+app.use(router.routes());
 
-server.use(router.routes());
-
-server.listen(config.appPort, config.appHost);
+app.listen(config.appPort, config.appHost);
 
 if (isDev) {
     console.log('现在处于开发环境~~!');
